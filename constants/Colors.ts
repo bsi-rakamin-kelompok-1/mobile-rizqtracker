7e1545ba079a752354bf01e1c7cfcc5aeab5c02f
Rihlan Lumenda <rihlanrihlan24@gmail.com>
2025-04-29 16:01:47 +0700

    fix: Correct formatting issue in tertiaryMuted color definition

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -5,7 +5,7 @@ export default {
   secondary: '#F0A33C',
   secondaryMuted: '#F2D6B8',
   tertiary: '#cca3ff',
-  tertiaryMuted: '	#dabcff',
+  tertiaryMuted: '#dabcff',
   tertiaryDark: '#bf8bff',
   warning: '#FF9800',
   error: '#FF3D00',
@@ -13,4 +13,39 @@ export default {
   dark: '#141518',
   gray: '#626D77',
   lightGray: '#D8DCE2',
+};
+
+export const transactionColors = {
+  topup: {
+    icon: '#61B7AA',
+    background: '#D6F5E8'
+  },
+  needs: {
+    icon: '#9C27B0',
+    background: '#F3E5F5',
+  },
+  shopping: {
+    icon: '#FF9800',
+    background: '#FFF3E0',
+  },
+  transport: {
+    icon: '#03A9F4',
+    background: '#E1F5FE',
+  },
+  bills: {
+    icon: '#E5989B',
+    background: '#FFEBEE',
+  },
+  transfer_of_wealth: {
+    icon: '#4CAF50',
+    background: '#E8F5E9',
+  },
+  transfer: {
+    icon: '#9C27B0',
+    background: '#F3E5F5',
+  },
+  default: {
+    icon: '#607D8B',
+    background: '#ECEFF1',
+  },
 };
